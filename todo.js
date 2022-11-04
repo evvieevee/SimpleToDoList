c806776ee5fc7025e94d1128a53003640419875c
evvieevee <eve.puurtinen00@gmail.com>
2022-11-04 18:00:45 +0200

    fixed code and added comments

--- todo.js
+++ todo.js
@@ -1,82 +1,77 @@
 const todoList = document.getElementById("toDoList");
 let toDoArray = []; 
 
-
+/**Tässä kohtaa tallennetaan LocalStorageen. */
 function updateLocalStorage() {
     window.localStorage.setItem("evenStorage", JSON.stringify(toDoArray));
 }
-
+/**Tässä kohtaa tiedot haetaan localstoragesta. Ja alustetaan visuaalisuus saadulla datalla. */
 (function populateToDoList() {
     const a = JSON.parse(window.localStorage.getItem('evenStorage'));
     if(a?.length > 0) a.forEach(x => toDoArray.push(x)); 
-    toDoArray.forEach((x) => addElement(x, true))
+    toDoArray.forEach((x) => addElement(x))
 })();
 
-
-function addElement(x, fromLocal = false) {
-    if(!fromLocal && document.getElementById("inputField").value.length < 3) {
+/**Tämä antaa virheilmoituksen tyhjästä tai liian lyhyestä vastauksesta. Sivulle pompsahtaa lappu, 
+ * joka ilmoittaa, että kenttä on tyhjä. Luo elementit ja määrittelee niiden ominaisuudet. 
+*/ 
+function addElement(storageObject) {
+    if(!storageObject && document.getElementById("inputField").value.length < 3) {
         document.getElementById("inputField").style.border = "3px solid red";
         alert("Tyhjä kenttä!")
         return;
     }
+// Tässä taski yksilöidään antamalla sille uniikki indexi. 
     let index = 1;
-    if(x?.id) {
-        index = x.id;
+    if(storageObject?.id) {
+        index = storageObject.id;
     }else if(toDoArray.length > 0) {
         index = Math.max(...toDoArray.map(x => x.id)) + 1;
     }
     const node = document.createElement("li");
     const param = document.createElement("p");
+    /// Tässä luodaan rivin poisto nappi, jonka tekstiksi määritellään "Poista"
     const btnDelete = document.createElement("button");
     btnDelete.innerText = "Poista";
-    
-    if(x) {
-        param.addEventListener("click", () => {
-            checkElement(param, index);
-        });
-        param.style.textDecoration = x.checked ? "line-through" : "unset";
-        param.appendChild(document.createTextNode(x.task))
-        node.appendChild(param);
-        btnDelete.addEventListener("click", () => {
-            removeElement(index, node);
-        });
+    // Tässä asetetaan teksti-elementille kuittaus ominaisuus.
+    param.addEventListener("click", () => checkElement(param, index));
+    // Tässä pistetään poisto nappi elementille poisto ominaisuus.
+    btnDelete.addEventListener("click", () => removeElement(index, node));
+
+    // Tässä saadaan aikaan yliviivaus klikatulle riville. 
+    // Eli kun riviä klikataan, rivi merkitään yliviivatuksi, eli tehdyksi.
+    if(storageObject) {
+        param.style.textDecoration = storageObject.checked ? "line-through" : "unset";
+        param.appendChild(document.createTextNode(storageObject.task))
     }else {
         const value = document.getElementById("inputField").value;
-        
         param.appendChild(document.createTextNode(value))
-        node.appendChild(param);
-        param.addEventListener("click", () => {
-            checkElement(param, index);
-        });
-        btnDelete.addEventListener("click", () => {
-            removeElement(index, node);
-        });
-
         toDoArray.push({id: index, task: value, checked: false});
 
+    // Päivitetään tiedot localstorageen, jonka jälkeen tyhjennetään input fieldi
         updateLocalStorage();
         document.getElementById("inputField").value = ""
     }
     
+    node.appendChild(param);
     node.appendChild(btnDelete);
-    todoList.appendChild(node);
-    
+    // lisätään elementit visuaalisesti listaan
+    todoList.appendChild(node);  
 }
-
+/** tämä poistaa elementin listasta kokonaan */
 function removeElement(removeId, node) {
     node.remove();
     toDoArray = toDoArray.filter(x => x.id !== removeId)
     updateLocalStorage();
 }
-
+/** Tämä muuttaa taskin kuittauksen arvon */
 function checkElement(node, index) {
     const found = toDoArray.find(element => element.id === index);
-    console.log(toDoArray, index);
     found.checked = !found.checked;
     node.style.textDecoration = found.checked ? "line-through" : "unset";
     updateLocalStorage();
 }
-
-function inputOnChange(){
+/** Poistaa inputfieldin punaisen reunan */
+function inputOnChange() {
     document.getElementById("inputField").style.border = "";
 } 
